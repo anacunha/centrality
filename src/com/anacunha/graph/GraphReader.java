2b7bd5b0ec3c62382901b663fe499432d02a34b8
Ana Luiza Cunha <mcunha.ana@gmail.com>
2016-11-21 00:54:20 -0200

    Add GraphReader utility according to graph file specifications

---
+++ src/com/anacunha/graph/GraphReader.java
@@ -0,0 +1,34 @@
+package com.anacunha.graph;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GraphReader {
+
+    /**
+     * Returns an undirected unweighted Graph from a file, where each
+     * line consists of two vertex names separated by a single space,
+     * representing an edge between those two nodes.
+     *
+     * @param fileName The filename of the file to be read.
+     * @return The Graph from the given file.
+     */
+    public static Graph readGraph(String fileName) {
+        Graph graph = new Graph();
+
+        // Adds an undirected edge for each line in file
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+
+            while (scanner.hasNext()){
+                String[] edges = scanner.nextLine().split("\\s+");
+                graph.addUndirectedEdge(Integer.valueOf(edges[0]), Integer.valueOf(edges[1]));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return graph;
+    }
+}
